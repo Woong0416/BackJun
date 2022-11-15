2a9cab060b0f589d07b415df444c7d489851f7b6
Woong0416 <99726996+Woong0416@users.noreply.github.com>
2022-11-15 13:53:47 +0900

    Create back NO.10952.cpp

---
+++ back NO.10952.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int A, B = 1 ;
+
+    while(A!=0 && B!=0) {
+
+        cin >> A >> B ;
+        if(A+B == 0)
+            break ;
+        else
+            cout << A+B << endl ;
+    }
+
+    return 0 ;
+}
